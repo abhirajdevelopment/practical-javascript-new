db21b8b6e0f395a9d81a3a90eca1570a63aa1ed2
Abhiraj Gaikwad <abhiraj.gaikwad@neosofttech.com>
2018-11-10 01:42:17 +0530

    version 2 complete

--- app/script.js
+++ app/script.js
@@ -1,23 +1,82 @@
 var gameList = {
-    games: ['Contra', 'Mario', 'NFS'],
+    games: [],
     displayGames: function () {
-        console.log("Games List:", this.games);
+        if (this.games.length === 0) {
+            console.log("You have no games in your collection.");
+        } else {
+            console.log("Games List:");
+            this.games.forEach(function (game) {
+                if (game.gameSold) {
+                    console.log("(x)", game.gameTitle);
+                } else {
+                    console.log("( )", game.gameTitle);
+                }
+            });
+        }
+        console.log("<----- End ----->");
     },
-    addGames: function (game) {
-        this.games.push(game);
+    addGame: function (gameTitle) {
+        this.games.push({
+            gameTitle: gameTitle,
+            gameSold: false
+        });
+
         this.displayGames();
     },
-    changeGame: function (position, newName) {
-        this.games[position] = newName;
-        this.displayGames();
+    changeGame: function (position, gameTitle) {
+        if (this.games[position] === undefined) {
+            console.log("No games found. Please add a game first.")
+        } else {
+            this.games[position].gameTitle = gameTitle;
+            this.displayGames();
+        }
     },
     deleteGame: function (position) {
-        this.games.splice(position, 1);
+        if (this.games[position] === undefined) {
+            console.log("No games found. Please add a game first.")
+        } else {
+            this.games.splice(position, 1);
+        }
+
+        this.displayGames();
+    },
+    toggleSold: function (position) {
+        if (this.games[position] === undefined) {
+            console.log("No games found. Please add a game first.")
+        } else {
+            var sold = this.games[position]
+            sold.gameSold = !sold.gameSold;
+        }
+
+        this.displayGames();
+    },
+    toggleAll: function () {
+        var totalGames = this.games.length;
+        var soldGames = 0;
+
+        this.games.forEach(function (game) {
+            if (game.gameSold === true) {
+                soldGames++;
+            }
+        });
+
+        this.games.forEach(function (game) {
+            if (soldGames === totalGames) {
+                game.gameSold = false;
+            } else {
+                game.gameSold = true;
+            }
+        });
+
         this.displayGames();
     }
 }
 
 gameList.displayGames();
-gameList.addGames('Spiderman');
-gameList.changeGame(2, 'NFS Most Wanted');
-gameList.deleteGame(gameList.games.length - 1);
+gameList.addGame('Mario');
+gameList.addGame('GTA: San Andreas');
+gameList.addGame('Counter Strike: Global Offensive');
+gameList.addGame('Angry Birds');
+gameList.changeGame(0, 'NFS');
+gameList.toggleSold(0);
+gameList.toggleAll();
